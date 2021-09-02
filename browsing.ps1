40d3ec1006aa1f09c188224d26bf5fb2a5d9e174
hunchob <alex.bobkov98@mail.ru>
2021-09-02 08:59:00 +0300

    Fix browser

--- browsing.ps1
+++ browsing.ps1
@@ -15,6 +15,7 @@ while( $ie.busy){Start-Sleep 1}
 $ie.Document.getElementById("email_field").value = $username
 $ie.Document.getElementById("password_field").value = $passwdPlain
 ($ie.Document.getElementsByTagName("button") | ? { $_.className -eq "button button_wide button_primary"}).click()
+while( $ie.busy){Start-Sleep 1} 
 # ----------------------------------------------------------------------
 # Start-Sleep -Seconds '15'
 # $ie.Navigate( 'google.com' )

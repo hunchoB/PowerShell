6b2a079c694a9124ad8d0487df50acd5fe7f3ae5
hunchob <alex.bobkov98@mail.ru>
2021-09-03 11:09:04 +0300

    Small fixes

--- archive.ps1
+++ archive.ps1
@@ -1,3 +1,3 @@
 $pathToFile = "C:\Users\a.bobkov\Desktop\events.csv.zip"
 Expand-Archive -Path $pathToFile -DestinationPath $pathToFile.Replace(".zip","")
-Remove-Item -Path $pathToFile -Force
+Remove-Item -Path $pathToFile

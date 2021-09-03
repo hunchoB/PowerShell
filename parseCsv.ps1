5db8183a8f54311e5c35e666b6addfabed0751c7
hunchob <alex.bobkov98@mail.ru>
2021-09-03 11:08:26 +0300

    Counter of events from CSV

---
+++ parseCsv.ps1
@@ -0,0 +1,20 @@
+$pathToFile = "C:\Users\a.bobkov\Desktop\events.csv\events.csv"
+$SearchItems = "icmp-event","check"
+
+$GrpObjResults = Import-Csv $pathToFile -Delimiter ";" | Where-Object {$_.Class -in $SearchItems} | Group-Object Class -NoElement
+
+$ObjProps = @{
+    Name = [String]
+    Count = [int]
+}
+
+$Results = @()
+
+Foreach($Term in $SearchItems){
+    $ListEntry = New-Object -TypeName PSObject -Property $ObjProps
+    $ListEntry.Name = $Term
+    $ListEntry.Count = ($GrpObjResults | Where-Object {$_.Name -eq $Term}).Count
+    $Results += $ListEntry
+}
+
+$Results

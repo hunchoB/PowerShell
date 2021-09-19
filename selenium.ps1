59cad1953b352e2e92a4c6a2b934eca662162c61
Huncho <alex.bobkov98@mail.ru>
2021-09-19 14:48:48 +0300

    Remade all actions from standart options of a IE to Selenium Chrome

---
+++ selenium.ps1
@@ -0,0 +1,33 @@
+$url = 'http://10.61.63.13/#ids_main_wrapper/events_tabs/search2Set'
+
+# ----------------Enter credentials----------------------------------
+# $username = Read-Host 'Enter Login'
+# $passwordSecureString = Read-Host 'Enter password' -AsSecureString
+# $passwdPlain = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+#     [Runtime.InteropServices.Marshal]::SecureStringToBSTR($passwordSecureString))
+# ---------------------------------------------------------------------
+
+
+# Import-Module "D:\MEGA\MEGAsync\code\PowerShell\WebDriver.dll"
+$chromeDriver = New-Object OpenQA.Selenium.Chrome.ChromeDriver
+$chromeDriver.Navigate().GoToUrl($url)
+$chromeDriver.FindElementById("textfield-1185-inputEl").SendKeys()
+$chromeDriver.FindElementById("textfield-1187-inputEl").SendKeys()
+$chromeDriver.FindElementById("button-1190-btnWrap").Click()
+
+$chromeDriver.FindElementByXPath("//a[@class='x-tab addNewReportSet-button x-unselectable x-tab-after-title x-box-item x-tab-default x-noicon x-tab-noicon x-tab-default-noicon x-top x-tab-top x-tab-default-top']").Click()
+
+$chromeDriver.FindElementByXPath("//input[@id='combobox-2570-inputEl']").Click()
+$chromeDriver.FindElementByXPath("//li[@class='add-new-filter']").Click() 
+$chromeDriver.FindElementByXPath("//input[@name='eventClass']").Click()
+
+$chromeDriver.FindElementByXPath("//li[contains(text(),'web-application-attack')]").Click()
+$chromeDriver.FindElementByXPath("//li[contains(text(),'attempted-dos')]").Click()
+$chromeDriver.FindElementByXPath("//li[contains(text(),'malware-cnc')]").Click()
+$chromeDriver.FindElementByXPath("//li[contains(text(),'trojan-activity')]").Click()
+$chromeDriver.FindElementByXPath("//a[@id='button-2672']").Click()
+# Start-Sleep -Seconds '15'
+# ----------Close Chrome and Stop ChromeDriverProcess----------
+# $chromeDriver.Close() 
+# $chromeDriver.Quit()
+# ------------------------------------------------------------- 
